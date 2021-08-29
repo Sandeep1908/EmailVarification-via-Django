1d633793ac25cfb8dd8e1fa26fa81248cd5e969e
sandeep <sandbro7163337@gmail.com>
2021-08-29 11:22:29 +0530

    email remove

--- Core/settings.py
+++ Core/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sandbro7163337@gmail.com'
-EMAIL_HOST_PASSWORD = 'sunderlalyadav'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
